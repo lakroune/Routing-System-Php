0b3447ca6d3857dbc0494205987f9dea391cc802
lakroune <lakroune00@gmail.com>
2026-01-12 23:01:08 +0100

    refactor: enhance RouteNotFoundException with a constructor for customizable messages; add documentation comments to HomeController methods for clarity

--- app/Exceptions/RouteNotFoundException.php
+++ app/Exceptions/RouteNotFoundException.php
@@ -2,6 +2,20 @@
 
 namespace App\Exceptions;
 
-class RouteNotFoundException extends \Exception{
+class RouteNotFoundException extends \Exception
+{
     protected $message = '404 Not Found';
+
+/**
+ * Constructor for RouteNotFoundException.
+ *
+ * @param string|null $message The message to set for the exception.
+ * If null, the default message will be used.
+ */
+    public function __construct($message = null)
+    {
+        if (null !== $message) {
+            $this->message = $message;
+        }
+    }
 }
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,17 +7,38 @@ use App\Models\User;
 
 class HomeController
 {
+    /**
+     * This function renders the home page.
+     * 
+     * @return View The rendered view
+     */
     public function index()
     {
         return View::make('home/index', ['message' => ' hello , i am home page']);
     }
 
+    /**
+     * Renders the users page.
+     * 
+     * This function renders the users page containing all users.
+     * 
+     * @return View The rendered view
+     */
     public function getAllUsers()
     {
         $userModel = new User(0, '', '', '');
         return View::make('home/users', ['users' => $userModel->getAll()]);
     }
 
+    /**
+     * Renders the user page.
+     * 
+     * This function renders the user page containing a user with the given ID.
+     * 
+     * @param int $id The ID of the user
+     * 
+     * @return View The rendered view
+     */
     public function getUser($id)
     {
         $userModel = new User(0, '', '', '');
