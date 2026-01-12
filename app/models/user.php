f8225909cacdebe7a74072d6b66ba0f2613d408a
lakroune <lakroune00@gmail.com>
2026-01-12 22:38:49 +0100

    refactor: update user instances in User model and simplify getOne method for improved data retrieval

--- app/models/user.php
+++ app/models/user.php
@@ -14,11 +14,11 @@ class User
     private static function data()
     {
         return [
-            new User(1, 'Ahmed', 'ahmed@gmail.com', 'test123'),
-            new User(2, 'Sara', 'sara@gmail.com', 'test123'),
+            new User(1, 'Ismail', 'ismail@gmail.com', 'test123'),
+            new User(2, 'Khalid', 'Khalid@gmail.com', 'test123'),
             new User(3, 'Ali', 'ali@gmail.com', 'test123'),
             new User(4, 'Omar', 'omar@gmail.com', 'test123'),
-            new User(5, 'Khalid', 'khalid@gmail.com', 'test123')
+            new User(5, 'Anass', 'anass@gmail.com', 'test123')
         ];
     }
 
@@ -29,12 +29,6 @@ class User
 
     public function getOne($id)
     {
-        return array_filter(self::data(), fn($item) => $item->id == $id)[0] ?? null;
-    }
-
-    public function create(array $data)
-    {
-        $newId = count(self::data()) + 1;
-        return new User($newId, $data['name'] ?? null, $data['email'] ?? null, $data['password'] ?? null);
+        return self::data()[$id - 1];
     }
 }
