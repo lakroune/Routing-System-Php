1a37b0683e779d1dc4e5f8652fec07b65bb4d645
lakroune <lakroune00@gmail.com>
2026-01-12 23:04:47 +0100

    refactor: add comprehensive documentation comments to View class methods, enhancing clarity and usability

--- core/View.php
+++ core/View.php
@@ -9,17 +9,41 @@ class View
     private $view;
     private $params;
 
+/**
+ * Construct a new View instance.
+ *
+ * @param string $view The view to render
+ * @param array $params The parameters to pass to the view
+ */
     public function __construct($view, $params)
     {
         $this->view = $view;
         $this->params = $params;
     }
 
+/**
+ * Creates a new View instance.
+ *
+ * @param string $view The view to render
+ * @param array $params The parameters to pass to the view
+ *
+ * @return View The created View instance
+ */
     public static function make($view, $params = [])
     {
         return new static($view, $params);
     }
 
+/**
+ * Renders the view.
+ *
+ * The view is rendered by including the view file and passing the
+ * parameters to it. The rendered view is then returned.
+ *
+ * @return string The rendered view
+ *
+ * @throws Exception If the view file is not found
+ */
     public function render()
     {
         ob_start();
@@ -34,6 +58,15 @@ class View
         return ob_get_clean();
     }
 
+
+/**
+ * Returns the rendered view as a string.
+ *
+ * This magic method is called when the View instance is converted to a string.
+ * It returns the rendered view as a string.
+ *
+ * @return string The rendered view
+ */
     public function __toString()
     {
         return $this->render();
